b03819b8e1e664e9d653881350dcf98b790a2cce
lee jae hoon <zxczoxc125@gmail.com>
2021-07-07 15:03:18 +0900

    # vue-bootstrap 적용, vue2로 다운그레이드

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue';
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+import Vue from 'vue';
+import './plugins/bootstrap-vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
