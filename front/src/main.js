240a97ede60e8b33a8abb330a97841951f5334e9
zxczoxc125 <zxczoxc125@gmail.com>
2021-07-14 14:33:09 +0900

    # 라우터 구조 세분화

--- front/src/main.js
+++ front/src/main.js
@@ -1,21 +1,11 @@
 import '@babel/polyfill';
 import 'mutationobserver-shim';
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import './plugins/bootstrap-vue';
-import './plugins/vue-router';
+import router from './plugins/vue-router';
 import App from './App.vue';
-import Layout from './components/Layout/Layout.vue';
-import SignIn from './views/SignIn.vue';
 
 Vue.config.productionTip = false;
-
-const router = new VueRouter({
-  routes: [
-    { path: '/sign-in', component: SignIn },
-    { path: '/', component: Layout },
-  ],
-});
 
 new Vue({
   render: (h) => h(App),
--- front/src/plugins/vue-router.js
+++ front/src/plugins/vue-router.js
@@ -1,4 +1,28 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Layout from '../components/Layout/Layout.vue';
+import SignIn from '../views/SignIn.vue';
+import NotFound from '../views/NotFound.vue';
+import Main from '../views/Main.vue';
 
 Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: SignIn },
+    {
+      path: '/',
+      component: Layout,
+      children: [
+        {
+          path: '/main',
+          component: Main,
+        },
+      ],
+    },
+    { path: '*', component: NotFound },
+  ],
+  mode: 'history',
+});
+
+export default router;
